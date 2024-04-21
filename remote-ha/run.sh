441e733e9363f6fac162f068d2749175e102e88b
khongpt <khongpt@gmail.com>
2024-04-21 21:49:45 +0700

    Update run.sh

--- remote-ha/run.sh
+++ remote-ha/run.sh
@@ -10,9 +10,9 @@ function stop_frpc() {
 serverip="ws3.airplaycar.com"
 port_number="8123"		
 key=""
-key=`cat /sys/class/net/docker0/address`		
-if [ "$key" == "" ]; then		
-  key=`ifconfig hassio | grep HWaddr | cut -d ' ' -f9`
+key=`ifconfig hassio | grep HWaddr | cut -d ' ' -f9`		
+if [ "$key" == "" ]; then
+   key=`cat /sys/class/net/docker0/address`
 fi
 lanip=`cat /etc/hostname | tr -d '\n'`		
 rid=$(bashio::config 'remoteID')		
